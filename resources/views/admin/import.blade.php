177beda5de6cb4b0c8b744bee2163c40c35bd70c
adzkyzaky <61900533+adzkyyy@users.noreply.github.com>
2021-09-21 21:49:02 +0700

    lupa csrf

--- resources/views/admin/import.blade.php
+++ resources/views/admin/import.blade.php
@@ -33,6 +33,7 @@
                                 <div class="modal fade" id="importExcel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                     <div class="modal-dialog" role="document">
                                         <form method="post" action="{{ route('import.siswa') }}" enctype="multipart/form-data">
+                                            @csrf
                                             <div class="modal-content">
                                                 <div class="modal-header">
                                                     <h5 class="modal-title" id="exampleModalLabel">Import Excel</h5>
