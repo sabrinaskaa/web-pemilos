6aa9a4edb8b1d33ffa41772a8a779bc0ecdcb3c9
sabrinaskaa <70563202+sabrinaskaa@users.noreply.github.com>
2021-09-22 20:38:33 +0700

    Update nav.blade.php

--- resources/views/partial/nav.blade.php
+++ resources/views/partial/nav.blade.php
@@ -1,5 +1,8 @@
 <header class=" mt-5 mb-5 l-header scroll-header" id="header">
     <nav class="nav bd-container nav-voting">
-        <h2 class="nav_title">PEMILIHAN CAKETOS</h2>
+        <h2 class="nav_title">
+            <span class="nav_title-1">PEMILIHAN KETUA OSIS </span>
+            <span class="nav_title-2">SMKN 7 SEMARANG PERIODE 2021/2022</span>
+        </h2>
     </nav> 
 </header>
