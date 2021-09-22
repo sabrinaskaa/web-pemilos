43c97cf31224023697a1a9d87091b559d7b7ba74
adzkyyy <adzkizaki24@gmail.com>
2021-09-22 22:31:50 +0700

    hapus form

--- resources/views/submit.blade.php
+++ resources/views/submit.blade.php
@@ -12,24 +12,17 @@
                 <span class="success-text">Terimakasih atas partisipasinya.</span>
             </div>
         </div>
-        <div class="success-text-row d-flex align-items-start mt-4">
-            <div class="col-md-5">
-                <form>
-                    <div class="mb-3" style="">
-                        <label for="name">Nama</label>
-                        <input type="text" class="form-control" id="name" placeholder="Ketik nama">
-                    </div>
-                    <div class="mb-3">
-                        <label for="pesan">Pesan</label>
-                        <textarea class="form-control" id="pesan" rows="3" placeholder="Ketik Pesan"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Kirim</button>
-                </form>
+        <div class="saran-text-row d-flex align-items-center">
+            <div class="saran-text-card mt-3">
+                <span class="kritik-saran">Kasih kata-kata mutiaranya dong</span>
+                <a href="https://forms.gle/h79vZ7tSuQpmiGs99">
+                <p class="link-gform mt-3" type="submit">https://forms.gle/h79vZ7tSuQpmiGs99</p>
+                </a>
             </div>
         </div>
         <footer class="mt-auto text-white- text-center fixed-bottom mb-4">
             <a href="https://youtu.be/dQw4w9WgXcQ"><button class="btn btn-danger">ADMIN-SITE</button></a>    
         </footer>
     </main>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 @endsection
